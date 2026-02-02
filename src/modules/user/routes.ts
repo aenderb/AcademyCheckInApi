862b042ed7a91e1bd9cdfffe2d760ed83d824805
aenderbinoto <aederb@hotmail.com>
2026-02-02 20:14:44 -0300

    feature: rate-limit + vitest

--- src/modules/user/routes.ts
+++ src/modules/user/routes.ts
@@ -2,6 +2,7 @@ import { Router } from "express";
 import { CreateUserController } from "./controller/CreateUserController";
 import { GetUserByIdController } from "./controller/GetUserByIdController";
 import { AuthenticateUserController } from "./controller/AuthenticateUserController";
+import { authLimiter, signupLimiter } from "@/shared/middlewares/rateLimiter";
 
 // User routes
 const routes = Router();
@@ -10,9 +11,9 @@ const createUserController = new CreateUserController();
 const getUserByIdController = new GetUserByIdController();
 const authenticateUserController = new AuthenticateUserController();
 
-routes.post("/signup", createUserController.handle);
+routes.post("/signup", signupLimiter, createUserController.handle);
 
-routes.post("/signin", authenticateUserController.handle);
+routes.post("/signin", authLimiter, authenticateUserController.handle);
 
 routes.get("/:id", getUserByIdController.handle);
 
